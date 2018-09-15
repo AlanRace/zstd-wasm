adb12918a7f53fc78c6e9db29c3e0b76e9839466
Alan Race <alan@alanrace.com>
2018-09-15 08:41:48 +0200

    Updated example with simple code and added helper functions

---
+++ src/zstd-helper.js
@@ -0,0 +1,67 @@
+function zstdCompressFloat32(data, level) {
+    if(!(data instanceof Float32Array))
+        data = new Float32Array(data);
+
+    return zstdCompress(data, level);
+}
+
+function zstdCompressFloat64(data, level) {
+    if(!(data instanceof Float64Array)) {
+        data = new Float64Array(data);
+    }
+
+    return zstdCompress(data, level);
+}
+
+function zstdCompress(data, level) {
+    let num_bytes = data.length * data.BYTES_PER_ELEMENT;
+    let dataPtr = Module._malloc(num_bytes);
+    let dataHeap = new Uint8Array(Module.HEAPU8.buffer, dataPtr, num_bytes);
+    dataHeap.set(new Uint8Array(data.buffer));
+
+    let num_bytes_compressed = Module.ZSTD_compressBound(num_bytes);
+    let compressedPtr = Module._malloc(num_bytes_compressed);
+    let compressedHeap = new Uint8Array(Module.HEAPU8.buffer, compressedPtr, num_bytes_compressed);
+
+    let ret = Module.ZSTD_compress(compressedHeap.byteOffset, num_bytes_compressed, dataHeap.byteOffset, num_bytes, level);
+
+    // Put result in new array so that we can free the memory
+    let compressedData = new Uint8Array(compressedHeap.subarray(0, ret));
+
+    // Free memory
+    Module._free(dataHeap.byteOffset);
+    Module._free(compressedHeap.byteOffset);
+
+    return compressedData;
+}
+
+function zstdDecompressFloat64(data) {
+    let floatData = zstdDecompress(data);
+
+    return new Float64Array(floatData.buffer, floatData.byteOffset);
+}
+
+function zstdDecompress(data) {
+    if(!data instanceof Uint8Array) {
+        data = Uint8Array(data);
+    }
+
+    let dataPtr = Module._malloc(data.length);
+    let dataHeap = new Uint8Array(Module.HEAPU8.buffer, dataPtr, data.length);
+    dataHeap.set(data);
+    
+    let num_bytes_decompressed = Module.ZSTD_getFrameContentSize(dataHeap.byteOffset, data.length); 
+    let decompressedPtr = Module._malloc(num_bytes_decompressed);
+    let decompressedHeap = new Uint8Array(Module.HEAPU8.buffer, decompressedPtr, num_bytes_decompressed);
+
+    let ret = Module.ZSTD_decompress(decompressedHeap.byteOffset, num_bytes_decompressed, dataHeap.byteOffset, data.length);
+
+    // Put result in new array so that we can free the memory
+    let decompressedData = new Uint8Array(decompressedHeap.subarray(0, ret));
+
+    // Free memory
+    Module._free(dataHeap.byteOffset);
+    Module._free(decompressedHeap.byteOffset);
+
+    return decompressedData;
+}
--- src/zstd-wasm.cpp
+++ src/zstd-wasm.cpp
@@ -12,49 +12,32 @@ using namespace emscripten;
 
 //https://github.com/kripken/emscripten/issues/6747
 size_t _ZSTD_compress(unsigned int output_ptr, int num_bytes, unsigned int input_ptr, int num_bytes_in, int level) {
-
 	return ZSTD_compress((uint8_t *) output_ptr, num_bytes, (uint8_t *) input_ptr, num_bytes_in, level);
 }
 
 size_t _ZSTD_decompress(unsigned int output_ptr, int num_bytes, unsigned int input_ptr, int num_bytes_in) {
-
 	return ZSTD_decompress((uint8_t *) output_ptr, num_bytes, (uint8_t *) input_ptr, num_bytes_in);
 }
+
+long _ZSTD_getFrameContentSize(unsigned int src, size_t srcSize) {
+	//TODO: THis function actually reutnrs long long
+	return ZSTD_getFrameContentSize((uint8_t *) src, srcSize);
+}
+
+
+
+//TODO: const char* ZSTD_getErrorName(size_t code);     /*!< provides readable string from an error code */
+
 
 EMSCRIPTEN_BINDINGS(zsd) {
 	function("ZSTD_compress", &_ZSTD_compress);
 	function("ZSTD_decompress", &_ZSTD_decompress);
-  function("ZSTD_maxCLevel", &ZSTD_maxCLevel);
+	function("ZSTD_getFrameContentSize", _ZSTD_getFrameContentSize);
+
+	// Helper functions
+	function("ZSTD_compressBound", &ZSTD_compressBound);
+	function("ZSTD_isError", &ZSTD_isError);
+    function("ZSTD_maxCLevel", &ZSTD_maxCLevel);
 	function("ZSTD_versionNumber", &ZSTD_versionNumber);
 }
 
-
-int main() {
-  double data[] = {100.123423, 999.2348238904};
-  double compressed[4];
-  double decompressed[2];
-  int len = 2;
-  int level = 3;
-
-  printf("hello, world!\n");
-
-  size_t rc = ZSTD_compress(&compressed, 32, &data, 16, level);
-
-  if(ZSTD_isError(rc)) {
-	printf("Error: %s\n", ZSTD_getErrorName(rc));
-
-	return 0;
-  }
-
-  rc = ZSTD_decompress(&decompressed, 16, &compressed, rc);
-
-  if(ZSTD_isError(rc)) {
-        printf("Error: %s\n", ZSTD_getErrorName(rc));
-
-        return 0;
-  }
-
-  printf("%f -> %f -> %f\n", data[0], compressed[0], decompressed[0]);
-
-  return 0;
-}
